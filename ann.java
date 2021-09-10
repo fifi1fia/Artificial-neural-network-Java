fc1e58085e3058cc93b5c7f12c3dee570a4cb939
fifi1fia <aafifaa@live.com>
2021-09-10 16:44:06 -0400

    initial commit

---
+++ ann.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ann;
+
+/**
+ *
+ * @author afifa
+ */
+public class ann {
+    public static void main(String []args){
+        Welcome2 w = new Welcome2();
+    }
+    
+}
